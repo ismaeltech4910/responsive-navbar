2e05feba7aa79830df867e3916b2b7300ce8b3bc
ismael abdulrauf <ismailtech11@icloud.com>
2023-03-13 23:13:38 +0000

    fixed style and few functionalities

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-const menuBtn = document.querySelector('.mobile-nav');
+const mobileBtn = document.querySelector('.mobile-toggle');
 const mobileNav = document.querySelector('.mobile-menu');
 
 
-function openNavigation() {
-    menuBtn.classList.toggle('.open');
-}
+mobileBtn.addEventListener('click', openNavigation);
 
-mobileNav.addEventListener('click', openNavigation);
+function openNavigation() {
+    mobileBtn.classList.toggle('open');
+    mobileNav.classList.toggle('open');
+};
+
+window.addEventListener('scroll', removeNav);
+
+function removeNav() {
+    mobileBtn.classList.remove('open');
+    mobileBtn.classList.remove('open');
+};
